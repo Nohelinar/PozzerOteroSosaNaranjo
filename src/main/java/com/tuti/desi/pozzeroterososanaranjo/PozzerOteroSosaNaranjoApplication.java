81a38f5c184aeece6d94679016e7a6f5490d840a
Noheli Naranjo <72586371+Nohelinar@users.noreply.github.com>
2026-06-09 23:18:50 -0300

    Epic 1: Corrección de dependencias y exclusión de DataSource para compilación exitosa

--- src/main/java/com/tuti/desi/pozzeroterososanaranjo/PozzerOteroSosaNaranjoApplication.java
+++ src/main/java/com/tuti/desi/pozzeroterososanaranjo/PozzerOteroSosaNaranjoApplication.java
@@ -2,8 +2,9 @@ package com.tuti.desi.pozzeroterososanaranjo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PozzerOteroSosaNaranjoApplication {
 
 	public static void main(String[] args) {
